aa5809b126fdd0611c84049bb347334ece855a60
Marek Novotný <marek.novotny@h2o.ai>
2021-02-15 16:33:38 +0100

    Temporary fixes

--- ml/src/main/scala/ai/h2o/sparkling/ml/features/H2OTargetEncoder.scala
+++ ml/src/main/scala/ai/h2o/sparkling/ml/features/H2OTargetEncoder.scala
@@ -55,7 +55,7 @@ class H2OTargetEncoder(override val uid: String)
       "seed" -> getNoiseSeed(),
       "training_frame" -> input.frameId)
     val targetEncoderModelId = trainAndGetDestinationKey(s"/3/ModelBuilders/targetencoder", params)
-    input.delete()
+    // input.delete() TODO: Uncomment once TargetEncoderModel stops pointing to training DS.
     val model = new H2OTargetEncoderModel(uid, H2OModel(targetEncoderModelId)).setParent(this)
     copyValues(model)
   }
